d773c205417fd9123d05201e854aa882679a5fc9
Frode Aarstad <frodeaarstad@gmail.com>
2025-11-05 14:10:15 +0100

    test: remove snapshot from well_rate test and make it run on lsf again (#104)

--- ci/testkomodo.sh
+++ ci/testkomodo.sh
@@ -9,20 +9,27 @@ run_tests () {
         echo "Unsupported runner label: $CI_RUNNER_LABEL"
         return 1
     fi
+
+    # Create a temp folder and copy the repo there
     mkdir -p "$RUNNER_ROOT"
     run_path=$(mktemp -d -p "$RUNNER_ROOT")
     chmod ugo+rx "$run_path"
     cp -rf . "$run_path"/everest-tutorials
     pushd "$run_path"/everest-tutorials
+
     # Need to create a komodoenv on a network mapped drive for LSF etc.
     source "${_KOMODO_ROOT}"/"${_FULL_RELEASE_NAME}"/enable
     komodoenv --root "${_KOMODO_ROOT}" -r "${_FULL_RELEASE_NAME}" --no-update --force test-kenv
     source test-kenv/enable
     check_queue "$CI_RUNNER_LABEL"
+
+    # Install the package
     pip install --upgrade pip
     pip install .
     release_name=$(echo "$_FULL_RELEASE_NAME" | cut --delimiter=- --fields=1)
     set +o errexit
+
+    # Run the tests
     python -m pytest -svv tests | tee pytest-output
     if [[ "$?" -eq "1" ]]; then
         # The failure return code should be masked if this is due to pytest timing out,
@@ -37,6 +44,7 @@ run_tests () {
         fi
     fi
     set -o errexit
+
     # Clean up the temp folder removing folders older than 7 days
     find "$RUNNER_ROOT" -maxdepth 1 -mtime +7 -user f_scout_ci -type d -exec rm -r {} \;
 }
