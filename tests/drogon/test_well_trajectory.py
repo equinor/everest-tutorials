ac32601b32b6b9228aca619a68afc32afb01d8ce
Frode Aarstad <frodeaarstad@gmail.com>
2025-11-10 12:40:59 +0100

    test: add more tests

---
+++ tests/drogon/test_well_trajectory.py
@@ -0,0 +1,19 @@
+from pathlib import Path
+import pytest
+from everest.bin.everest_script import everest_entry
+
+def test_well_trajectory_simulation(capsys):
+    """
+    Run a basic Drogon well_trajectory optimization test case.
+    """
+
+    config_path = Path("data/drogon/well_trajectory/everest/model/welltrajectory_experiment.yml")
+    config_path.write_text(config_path.read_text().replace('max_batch_num: 10', 'max_batch_num: 2'))
+    
+    try:
+        everest_entry([str(config_path), "--skip-prompt"])
+    except SystemExit as e:
+        pytest.fail(f"Everest exited with SystemExit: {e}")
+
+    captured = capsys.readouterr()    
+    assert "Everest run finished with" in captured.out
