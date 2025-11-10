5309f18531e5656b1af1f83a51c2873d6614bfba
Frode Aarstad <frodeaarstad@gmail.com>
2025-11-10 14:09:26 +0100

    test: add egg test and fix egg.yml (#106)

---
+++ tests/egg/test_egg.py
@@ -0,0 +1,14 @@
+import pytest
+from everest.bin.everest_script import everest_entry
+
+def test_egg_simulation(capsys):
+    """
+    Run a basic egg simulation test case.
+    """
+    try:
+        everest_entry(["data/egg/everest/model/egg.yml", "--skip-prompt"])
+    except SystemExit as e:
+        pytest.fail(f"Everest exited with SystemExit: {e}")
+
+    captured = capsys.readouterr()    
+    assert "Everest run finished with" in captured.out
