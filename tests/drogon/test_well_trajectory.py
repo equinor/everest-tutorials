c9a4432e79d868256edd22c3f66e3a59afb3ce5b
Frode Aarstad <frodeaarstad@gmail.com>
2025-11-12 18:29:55 +0100

    test: run flow to generate files needed for the test (#112)

--- tests/drogon/test_well_trajectory.py
+++ tests/drogon/test_well_trajectory.py
@@ -1,14 +1,36 @@
 from pathlib import Path
 import pytest
+import os
 from everest.bin.everest_script import everest_entry
+from ert.resources.forward_models import run_reservoirsimulator
+
+
 
 def test_well_trajectory_simulation(capsys):
     """
     Run a basic Drogon well_trajectory optimization test case.
+
+    Modify to run flow sims for first 10 realizations only and
+    reduce the max_batch_num to 2 for faster testing.
     """
 
+    # First run flow simulations for the first ten realizations to generate necessary data files
+    for realization in range(10):
+        path = f"data/drogon/fmu-drogon-flow-files/realization-{realization}/iter-0/eclipse/model"
+        original_path = os.getcwd()
+        os.chdir(path)
+        erun = run_reservoirsimulator.RunReservoirSimulator(
+            simulator="flow", version="default", ecl_case=f"DROGON-{realization}.DATA"
+        )
+        erun.run_flow()
+        os.chdir(original_path)
+
     config_path = Path("data/drogon/well_trajectory/everest/model/welltrajectory_experiment.yml")
-    config_path.write_text(config_path.read_text().replace('max_batch_num: 10', 'max_batch_num: 2'))
+    config_path.write_text(
+        config_path.read_text()
+        .replace('max_batch_num: 10', 'max_batch_num: 2')
+        .replace('realizations: 0-99', 'realizations: 0-9')
+    )
     
     try:
         everest_entry([str(config_path), "--skip-prompt"])
